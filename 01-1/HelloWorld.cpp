57d48519c6e69e95f9d822935622d401a4459cf3
knhoooo <knh000125@hanyang.ac.kr>
2024-09-05 23:20:07 +0900

    Create HelloWorld.cpp

---
+++ 01-1/HelloWorld.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+//헤더 파일 선언
+
+int main()
+{
+    int num = 20;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello " << "World!" << std::endl;
+    std::cout << num << ' ' << 'A';
+    std::cout << ' ' << 3.14 << std::endl;
+    //std::cout 출력
+    //std::endl 개행
+    return 0;
+}
