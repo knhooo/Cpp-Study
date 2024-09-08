61310718026dc59cf25a8c0bee265feb1166354f
knhooo <knh000125@hanyang.ac.kr>
2024-09-08 15:54:24 +0900

    01-2 함수 오버로딩

---
+++ 01-2/FunctionOverloading.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void MyFunc(void) { //1번
+    std::cout << "MyFunc(void) called" << std::endl;
+}
+
+void MyFunc(char c) { //2번
+    std::cout << "MyFunc(char c) called" << std::endl;
+}
+
+void MyFunc(int a, int b) { //3번
+    std::cout << "MyFunc(int a, int b) called" << std::endl;
+}
+
+int main()
+{
+    MyFunc(); //1번
+    MyFunc('A'); //2번
+    MyFunc(12, 13); //3번
+    return 0;
+}
+
