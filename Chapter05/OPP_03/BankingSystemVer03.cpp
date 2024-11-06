6be4d939c04931e843f5ebcfc668a3d8504e4fef
knhooo <knh000125@hanyang.ac.kr>
2024-11-06 14:18:29 +0900

    BankingSystem, 07-1

---
+++ Chapter05/OPP_03/BankingSystemVer03.cpp
@@ -0,0 +1,111 @@
+﻿//Account 클래스에 깊은 복사를 진행하는 복사 생성자를 정의하면서,
+//Banking System의 버전을 0.2에서 0.3으로 업그레이드 시켜보자.
+//복사 생성자가 호출되지는 않지만, 깊은 복사를 원칙적으로 정하고, 이를 위해서 복사 생성자를 추가한다.
+#include <iostream>
+
+using std::string;
+
+int num = 0;
+int id[30];
+string name[30];
+int money[30];
+
+void CreateAccount() { //계좌 개설
+
+    std::cout << "계좌ID: ";
+    std::cin >> id[num];
+    std::cout << "이 름: ";
+    std::cin >> name[num];
+    std::cout << "입금액: ";
+    std::cin >> money[num];
+
+    num++;
+}
+
+void Deposit() {//입금
+    int inserted_id;
+    int deposit;
+    int arr_num = 0;
+
+    std::cout << "[입   금]" << std::endl;
+    std::cout << "계좌ID: ";
+    std::cin >> inserted_id;
+    std::cout << "입금액: ";
+    std::cin >> deposit;
+    std::cout << "입금 완료";
+    std::cout << " " << std::endl;
+
+    for (int i = 0; i < sizeof(id); i++) {
+        if (id[i] == inserted_id) {
+            arr_num = i;
+        }
+    }
+
+    money[arr_num] += deposit;
+}
+
+void Withdraw() {//입금
+    int inserted_id;
+    int withdraw;
+    int arr_num = 0;
+
+    std::cout << "[출   금]" << std::endl;
+    std::cout << "계좌ID: ";
+    std::cin >> inserted_id;
+    std::cout << "출금액: ";
+    std::cin >> withdraw;
+    std::cout << "출금 완료";
+    std::cout << " " << std::endl;
+
+    for (int i = 0; i < sizeof(id); i++) {
+        if (id[i] == inserted_id) {
+            arr_num = i;
+        }
+    }
+
+    money[arr_num] -= withdraw;
+}
+
+void PrintAll() {//계좌정보 전체 출력
+    for (int i = 0; i < sizeof(id); i++) {
+        if (id[i] == 0) break;
+        std::cout << " " << std::endl;
+        std::cout << "계좌ID: " << id[i] << std::endl;
+        std::cout << "이 름: " << name[i] << std::endl;
+        std::cout << "잔 액: " << money[i] << std::endl;
+    }
+}
+
+int main()
+{
+    int insertNum;
+    while (true) {
+        std::cout << " " << std::endl;
+        std::cout << "-----Menu-----" << std::endl;
+        std::cout << "1. 계좌개설" << std::endl;
+        std::cout << "2. 입 금" << std::endl;
+        std::cout << "3. 출 금" << std::endl;
+        std::cout << "4. 계좌정보 전체 출력" << std::endl;
+        std::cout << "5. 프로그램 종료" << std::endl;
+        std::cout << "선택: ";
+        std::cin >> insertNum;
+        std::cout << " " << std::endl;
+
+        switch (insertNum)
+        {
+        case 1: CreateAccount();
+            break;
+        case 2: Deposit();
+            break;
+        case 3: Withdraw();
+            break;
+        case 4: PrintAll();
+            break;
+        case 5: return 0; //프로그램 종료
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
---
+++ Chapter06/06-4/BankingSystemVer04.cpp
@@ -0,0 +1,156 @@
+﻿//Accout 클래스의 멤버함수 중 일부를 const로 선언하면서
+//BankingSystem의 버전을 업그레이드 시키겠다.
+//const로 선언이 가능한 모든 멤버함수를 const로 선언하자.
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Account {
+private:
+	int id;//계좌번호
+	char* name;//이름
+	int money;//잔액
+
+public:
+	Account(int pid, char* pname, int pmoney)//생성자
+		:id(pid), money(pmoney)
+	{
+		name = new char[strlen(pname) + 1];
+		strcpy(name, pname);
+	}
+	Account(const Account& ref)//복사 생성자
+		:id(ref.id), money(ref.money)
+	{
+		name = new char[strlen(ref.name) + 1];
+		strcpy(name, ref.name);
+	}
+	 int GetID() const {
+		return id;
+	}
+	char* GetName() const {
+		return name;
+	}
+	int GetMoney() const {
+		return money;
+	}
+	int Withdraw(int num) {
+		if (num > money) {
+			return 0;
+		}
+		money -= money;
+		return money;
+	}
+	void Deposit(int num) {
+		money += num;
+	}
+	~Account() {//소멸자
+		delete[]name;
+	}
+};
+
+Account* AccountArr[100];
+int accNum = 0;
+
+void ShowMenu() {
+	cout << "-----Menu-----" << endl;
+	cout << "1. 계좌 개설" << endl;
+	cout << "2. 입     금" << endl;
+	cout << "3. 출     금" << endl;
+	cout << "4. 계좌정보 전체 출력" << endl;
+	cout << "5. 프로그램 종료" << endl;
+	cout << " " << endl;
+}
+
+void CreateAccount() {
+	int id;
+	char name[20];
+	int money;
+
+	cout << "[계좌개설]" << endl;
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "이름: ";
+	cin >> name;
+	cout << "입금액: ";
+	cin >> money;
+	//객체 생성
+	AccountArr[accNum++] = new Account(id, name, money);
+	cout << " " << endl;
+}
+void Deposit() {
+	int id;
+	int money;
+	cout << "[입   금]" << endl;
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "입금액: ";
+	cin >> money;
+	//입금 처리
+	for (int i = 0; i < accNum; i++) {
+		if (AccountArr[i]->GetID() == id) {
+			AccountArr[id]->Deposit(money);
+			cout << "입금 완료: " << endl << endl;
+			return;
+		}
+	}
+	cout << "유효하지 않은 ID 입니다." << endl << endl;
+}
+
+void WithDraw() {
+	int id;
+	int money;
+	cout << "[출   금]" << endl;
+	cout << "계좌ID: ";
+	cin >> id;
+	cout << "출금액: ";
+	cin >> money;
+	//출금 처리
+	for (int i = 0; i < accNum; i++) {
+		if (AccountArr[i]->GetID() == id) {
+			if (AccountArr[id]->Withdraw(money) == 0) {
+				cout << "잔액부족" << endl << endl;
+				return;
+			}
+			cout << "출금 완료: " << endl << endl;
+			return;
+		}
+	}
+	cout << "유효하지 않은 ID 입니다." << endl << endl;
+}
+void PrintAll() {
+	for (int i = 0; i < accNum; i++) {
+		cout << "계좌ID: " << AccountArr[i]->GetID() << endl;
+		cout << "이름: " << AccountArr[i]->GetName() << endl;
+		cout << "잔액: " << AccountArr[i]->GetMoney() << endl;
+		cout << " " << endl;
+	}
+}
+
+int main() {
+	int insert_num;
+	while (true) {
+		ShowMenu();
+		cout << "선택: ";
+		cin >> insert_num;
+		cout << " " << endl;
+		switch (insert_num) {
+		case 1: CreateAccount();
+			break;
+		case 2: Deposit();
+			break;
+		case 3: WithDraw();
+			break;
+		case 4: PrintAll();
+			break;
+		case 5:
+			for (int i = 0; i < accNum; i++)
+				delete AccountArr[i];
+			return 0;
+		default:
+			cout << "다시 입력하세요.." << endl;
+		}
+	}
+	return 0;
+}
